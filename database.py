db247073f178587a4bca8f97621506f11dac0b38
marjan <marjantavasoli73@gmail.com>
2026-06-17 17:03:32 +0330

    async database module

--- database.py
+++ database.py
@@ -1,24 +1,27 @@
-from sqlalchemy.orm.session import sessionmaker
-from sqlmodel import create_engine, SQLModel,Session
-from contextlib import contextmanager
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import  SQLModel
+from contextlib import asynccontextmanager
 
 
-SQLITE_DATABASE = "sqlite:///./app.db"
-engine = create_engine(SQLITE_DATABASE,connect_args={'check_same_thread': False})
+SQLITE_DATABASE = "sqlite+aiosqlite:///./app.db"
+engine = create_async_engine(SQLITE_DATABASE,connect_args={'check_same_thread': False})
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
-def create_tables():
-    SQLModel.metadata.create_all(engine)
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
 
 
 
-def get_session():
-    with Session(engine) as session:
+async def get_session():
+    async with async_session() as session:
         yield session
 
 
 
-@contextmanager
-def life_span():
-    create_tables()
+@asynccontextmanager
+async def lifespan(app):
+    await create_tables()
     yield
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from database import life_span
+from database import lifespan
 
 
-app = FastAPI(life_span=life_span)
+app = FastAPI(life_span=lifespan)
